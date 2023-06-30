0cf9ed9cd16daba34c4bf424c9b446ad95d1001e
BusolaAremu <busolaaremu@gmail.com>
2023-06-30 11:12:23 -0700

    0 correctiom

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,16 +2,16 @@
 /**
  * _strcat - concatenates two strings
  *
- * @dest: the string @src is added to
+ * @destt: the string @src is added to
  * @src: the string to be appended
  *
- * Return: pointer to the resulting string @dest
+ * Return: pointer to the resulting string @destt
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *destt, char *src)
 {
 	int len1; /* string length for src */
-	int len2;
+	int len2; /* string length for destt */
 	int i;
 
 	len1 = 0;
@@ -20,13 +20,13 @@ char *_strcat(char *dest, char *src)
 	while (src[len1] != '\0')
 		len1++;
 
-	while (dest[len2] != '\0')
+	while (destt[len2] != '\0')
 		len2++;
 
 	for (i = 0; i <= len1; i++)
 	{
-		dest[len2] = src[i];
+		destt[len2] = src[i];
 		len2++;
 	}
-	return (dest);
+	return (destt);
 }
