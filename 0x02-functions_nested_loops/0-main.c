0a890b7c6cfc9d2bb99daed031f4f997d89a5924
BusolaAremu <busolaaremu@gmail.com>
2023-07-04 07:27:33 +0100

    Update 0-main.c

--- 0x02-functions_nested_loops/0-main.c
+++ 0x02-functions_nested_loops/0-main.c
@@ -1,22 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * main - calls the function _putchar
  *
- * Description: programs that prints Holberton, followed by a new line
+ * Description: programs that prints _Putchar, followed by a new line
  * Return: 0
  */
 
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
