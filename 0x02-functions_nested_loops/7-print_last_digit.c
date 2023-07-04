eb9a3fe3fcf11b97f872b7615de32b3d3712e77b
BusolaAremu <busolaaremu@gmail.com>
2023-07-04 15:54:39 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,18 @@
 
 int print_last_digit(int n)
 {
-	int last = n % 10;
+	int a;
 
-	if (last < 0)
-		last = last * -1;
+	if (n < 0)
+		n = -n;
 
-	_putchar('0' + last);
-	return (last);
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+
 }
