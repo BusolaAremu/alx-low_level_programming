d19a7b99e82e2302fc61d6c55771f23d0db0e293
BusolaAremu <busolaaremu@gmail.com>
2023-07-04 16:01:16 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,47 +5,34 @@
  */
 void times_table(void)
 {
-	int i, j, p;
-	int k = 0;
+	int i, j, k;
+
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 11; j++)
+		for (j = 0; j < 10; j++)
 		{
-			p = j * k;
-			if (p >= 0 && p <= 9)
+			k = j * i;
+			if (j == 0)
 			{
-				if (j == 0)
-					_putchar(p + '0');
-				else if (j == 10)
-				{
-					_putchar('\n');
-					k++;
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(p + '0');
-				}
-			}
-			else if (p >= 10)
-			{
-				if (j == 10)
-				{
-					_putchar('\n');
-					k++;
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((p / 10) + '0');
-					_putchar((p % 10) + '0');
-				}
+				_putchar(k + '0');
 			}
 
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
